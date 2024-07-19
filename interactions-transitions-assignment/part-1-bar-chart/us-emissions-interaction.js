bdf5fa3d4e4bee362da9d04e8f00f5c91030ac1c
Dylan Blake <DBlake@mathematica-mpr.com>
2024-07-19 16:07:40 -0400

    finally got the graph to display and the npm run command to work as intended. had to change up the package.json in a way that differed from my peers but at least it is working.

--- interactions-transitions-assignment/part-1-bar-chart/us-emissions-interaction.js
+++ interactions-transitions-assignment/part-1-bar-chart/us-emissions-interaction.js
@@ -1,4 +1,10 @@
 function createChart(elementId) {
+    const toNum = stringNum => {
+        if (typeof stringNum === "string")
+            return +(stringNum.replace(/,/g, ''))
+        return stringNum;
+    };
+
     // container dimensions
     const height = 500;
     const width = 1000;
@@ -20,12 +26,75 @@ function createChart(elementId) {
     const g = svg
         .append('g')
         .attr('class', 'svg-group')
-        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
+        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')')
+        .attr('text-anchor', 'middle')
+        .attr('dominant-baseline', 'middle');
 
     // read in air quality data
-    d3.csv('air_quality.csv').then(function (data) {
+    d3.csv('air_quality.csv').then( function(data) {
+
+
         // bar chart and transitions code goes here
+
+        // const minEmissions = d3.min(data, d => +d.Emissions);
+        const maxEmissions = d3.max(data, d => toNum(d.Emissions));
+        const y = d3.scaleLinear()
+            .domain([0, maxEmissions])
+            .range([innerHeight, 0]);
+        const x = d3.scaleBand()
+            .domain(data.map(d => d.State))
+            .rangeRound([0, innerWidth])
+            .padding(0.1);
+
+        const regions = data.reduce((arr, state) => {
+            if (!arr.includes(state.Region)) arr.push(state.Region);
+            return arr;
+        }, new Array());
+
+        const scaleOrdinal = d3.scaleOrdinal()
+            .domain(regions)
+            .range(d3.schemeTableau10);
+
+        const chart = g.selectAll('g')
+            .data(data)
+            .join('g')
+            .attr('transform', d =>
+                `translate(${x(d.State)}, ${y(toNum(d.Emissions))})`);
+
+        const bars = chart.append('rect')
+            .attr('width', x.bandwidth())
+            .attr('fill', d => scaleOrdinal(d.Region))
+            .attr('height', d => innerHeight - y(toNum(d.Emissions)));
+
+        const bottomAxis = g.append('g')
+            .attr('transform', `translate(0,${innerHeight})`);
+
+        bottomAxis.append('g')
+            .call(d3.axisBottom(x));
+        bottomAxis.append('text')
+            .attr('font-size', 15)
+            .attr('x', +svg.attr('width') / 2)
+            .attr('y', 35)
+            .text('States');
+
+        const leftAxis = g.append('g');
+            leftAxis.append('g')
+            .call(d3.axisLeft(y));
+        leftAxis.append('text')
+            .attr('font-size', 15)
+            .attr('y', -10)
+            .text('Emissions');
+
+        const title = g.append('text')
+            .attr('font-size', 25)
+            .attr('x', (width - margins.left) / 2)
+            .attr('y', -10)
+            .text('Gas Emissions in 2020 Across the US');
+
+        console.log(maxEmissions);
     });
+    
+    return svg.node();
 }
 
 createChart('#chart-container');
