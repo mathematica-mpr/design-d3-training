4d0b8bbb56df0d9b11e7c07481df48c84ba845f8
Evelyn Zheng <evelynz@andrew.cmu.edu>
2024-07-19 15:52:25 -0400

    dog

--- interactions-transitions-assignment/part-1-bar-chart/us-emissions-interaction.js
+++ interactions-transitions-assignment/part-1-bar-chart/us-emissions-interaction.js
@@ -1,147 +1,178 @@
 function createChart(elementId) {
+
     // container dimensions
     const height = 500;
     const width = 1000;
     const margins = {
-        top: 50,
-        right: 50,
-        bottom: 50,
-        left: 50,
+      top: 50,
+      right: 50,
+      bottom: 50,
+      left: 50,
     };
-
+  
     // calculate dimensions without margins
     const innerHeight = height - margins.top - margins.bottom;
     const innerWidth = width - margins.left - margins.right;
-
+  
     // create svg element
-    const svg = d3.select(elementId).append('svg').attr('height', height).attr('width', width);
-
+    const svg = d3
+      .select(elementId)
+      .append('svg')
+      .attr('height', height)
+      .attr('width', width);
+  
     // create inner group element
     const g = svg
-        .append('g')
-        .attr('class', 'svg-group')
-        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
-    
-    const sortData = [
-        {
-            key: 'sort-state',
-            comp: (a,b) => d3.ascending(a.State, b.State)
-        },
-        {
-            key: 'sort-emissions-asc',
-            comp: (a,b) => d3.ascending(a.Emissions, b.Emissions)
-        },
-        {
-            key: 'sort-emissions-des',
-            comp: (a,b) => d3.descending(a.Emissions, b.Emissions)
-        }
-    ]
-    // read in air quality data
+      .append('g')
+      .attr('class', 'svg-group')
+      .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
+  
+
+    // Read in air quality data
     d3.csv('air_quality.csv').then(function (data) {
+        
         data.forEach(d => {
             d.Emissions = parseFloat(d.Emissions.replace(/,/g, ""));
         });
 
         const xScale = d3.scaleBand()
             .domain(data.map(d => d.State))
-            .range([0,width*0.8])
-            .padding(0.2)
+            .range([0, innerWidth])
+            .padding(0.2);
 
         const yScale = d3.scaleLinear()
-            .domain([0,d3.max(data, d => +d.Emissions)])
-            .range([height,0]);
+            .domain([0, d3.max(data, d => d.Emissions)])
+            .nice()
+            .range([innerHeight, 0]);
 
         const regionKey = d3.scaleOrdinal()
             .domain(data.map(d => d.Region))
             .range(d3.schemeSet2);
 
-        svg.selectAll('rect')
+        const bars = g.selectAll('.bar')
             .data(data)
             .enter()
             .append('rect')
-            .attr("transform", `translate(${margins.left}, -${margins.bottom})`)
+            .attr('class', 'bar')
             .attr('x', d => xScale(d.State))
-            .attr('y', d => yScale(+d.Emissions))
+            .attr('y', d => yScale(d.Emissions))
             .attr('width', xScale.bandwidth())
-            .attr('height', d => height - yScale(+d.Emissions))
-            .attr('fill', d => regionKey(d.Region))
+            .attr('height', d => innerHeight - yScale(d.Emissions))
+            .attr('fill', d => regionKey(d.Region));
+
     
-        svg.append('g')
-            .attr("transform", `translate(${margins.left}, ${height - margins.bottom})`)
+        g.append('g')
+            .attr('class', 'x-axis')
+            .attr('transform', `translate(0, ${innerHeight})`)
             .call(d3.axisBottom(xScale))
-            .attr("class", "x-axis")
-            .attr("font-size","7px")
+            .selectAll('text')
+                .attr('transform', 'rotate(-45)')
+                .attr('text-anchor', 'end')
+                .attr('dx', '-0.8em')
+                .attr('dy', '0.15em');
 
-        svg.append('g')
-            .attr("transform", `translate(${margins.left}, -${margins.bottom})`)
-            .call(d3.axisLeft(yScale))
-            .attr('class','y-axis')
+       
+        g.append('g')
+            .attr('class', 'y-axis')
+            .call(d3.axisLeft(yScale));
 
-        svg.append("text")
-            .attr("class", "x label")
-            .attr("text-anchor", "end")
-            .attr("x", width/2)
-            .attr("y", height)
-            .text("State")
-        .style("font-size","13px")
-        
-        svg.append("text")
-            .attr("class", "y label")
-            .attr("text-anchor", "end")
-            .attr("x", -100)
-            .attr("y", height)
-            .attr("transform", `translate(-${width/2},100) rotate(-90)`)
-            .text("Emissions")
-        .style("font-size","13px");
-        
-        svg.append("text")
-            .attr("class", "title")
-            .attr("text-anchor", "end")
-            .attr("x", (width/2)+margins.left)
-            .attr("y", 10)
-            .text("Emissions in the United States")
-            .style("font-size","14px")
+        // Labels
+        g.append('text')
+            .attr('class', 'x label')
+            .attr('text-anchor', 'end')
+            .attr('x', innerWidth / 2)
+            .attr('y', height)
+            .text('State')
+            .style('font-size', '13px');
 
+        g.append('text')
+            .attr('class', 'y label')
+            .attr('text-anchor', 'end')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', -margins.left + 5) 
+            .attr('x', -innerHeight / 2)
+            .text('Emissions')
+            .style('font-size', '13px');
 
-        const legendRectSize = 15
-        const legendSpacing = 5
+        g.append('text')
+            .attr('class', 'title')
+            .attr('text-anchor', 'end')
+            .attr('x', (width / 2) + margins.left)
+            .attr('y', 10)
+            .text('Emissions in the United States')
+            .style('font-size', '14px');
 
-        const legend = svg.append("g")
-            .attr("class","legend")
-            .attr("transform",`translate(${width-margins.right}`)
+        // Legend
+        const legendRectSize = 15;
+        const legendSpacing = 5;
 
-        const legendRegions = regionKey.domain().map((d,i) => ({
+        const legend = svg.append('g')
+            .attr('class', 'legend')
+            .attr('transform', `translate(${width - margins.right*2}, ${margins.top})`);
+
+        const legendRegions = regionKey.domain().map((d, i) => ({
             label: d,
             color: regionKey(d),
-            y: i*(legendRectSize + legendSpacing)
-            }))
+            y: i * (legendRectSize + legendSpacing)
+        }));
 
-        legend.selectAll("rect")
+        legend.selectAll('rect')
             .data(legendRegions)
             .enter()
-            .append("rect")
-            .attr("x", 120)
-            .attr("y", d => d.y)
-            .attr("width", legendRectSize)
-            .attr("height", legendRectSize)
-            .attr("fill", d => d.color)
+            .append('rect')
+            .attr('x', 0)
+            .attr('y', d => d.y)
+            .attr('width', legendRectSize)
+            .attr('height', legendRectSize)
+            .attr('fill', d => d.color);
 
-        legend.selectAll("text")
+        legend.selectAll('text')
             .data(legendRegions)
             .enter()
-            .append("text")
-            .attr("x", legendRectSize + legendSpacing + 120)
-            .attr("y", d => d.y + legendRectSize - 4)
-            .attr("text-anchor","start")
+            .append('text')
+            .attr('x', legendRectSize + legendSpacing)
+            .attr('y', d => d.y + legendRectSize - 4)
+            .attr('text-anchor', 'start')
             .text(d => d.label)
-            .style("font-size","13px")
+            .style('font-size', '13px');
 
-        d3.selectAll('.inputSort').on('input', ()=> {
-            const s = sortData.filter(d => d.key == this.id)[0]
-            data.sort(s.comp)
-            xScale.domain(data.map(d => d.State))
-        })
-            });
+        // Sort 
+        d3.selectAll('.sortTypes').on('input', function () {
+            function findComp(id) {
+                if (id === 'sort-states') {
+                    return (a, b) => d3.ascending(a.State, b.State);
+                } else if (id === 'sort-emissions-asc') {
+                    return (a, b) => d3.ascending(a.Emissions, b.Emissions);
+                } else {
+                    return (a, b) => d3.descending(a.Emissions, b.Emissions);
+                }
+            }
+            
+            data.sort(findComp(this.id));
+            xScale.domain(data.map(d => d.State));
+
+            const delay = +d3.select('#delay-input').node().value || 0;
+            if (isNaN(delay)) {
+                alert(`Please enter a valid number`);
+            } else {
+                bars.transition()
+                    .duration(1500)
+                    .attr('x', d => xScale(d.State))
+                    .delay((d, i) => i * delay);
+
+                g.select('.x-axis')
+                    .transition()
+                    .duration(1500)
+                    .call(xAxis)
+                    .selectAll('text') // rotate and adjust text labels
+                        .attr('transform', 'rotate(-45)')
+                        .attr('text-anchor', 'end')
+                        .attr('dx', '-0.7em')
+                        .attr('dy', '0.15em')
+                    .delay((d, i) => i * delay);
+            }
+        });
+    });
 }
 
 createChart('#chart-container');
