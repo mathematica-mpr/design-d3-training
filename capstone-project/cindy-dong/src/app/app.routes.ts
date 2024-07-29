20512a202c7610b4b04b19b4cc01814ee44f4554
Cindy Dong <cindyydong02@gmail.com>
2024-07-29 18:26:26 -0500

    Set up Line chart

--- capstone-project/cindy-dong/src/app/app.routes.ts
+++ capstone-project/cindy-dong/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
     },
+    { path: 'first-viz', component: LineChartComponent },
 ];
 
 @NgModule({
