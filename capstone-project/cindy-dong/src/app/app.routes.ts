ca664fceba78cd6d2c9adf3afb0035530aad37d9
Cindy Dong <cindyydong02@gmail.com>
2024-07-29 14:24:15 -0500

    Created landing page

--- capstone-project/cindy-dong/src/app/app.routes.ts
+++ capstone-project/cindy-dong/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ capstone-project/cindy-dong/src/app/line-chart/line-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-line-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './line-chart.component.html',
+  styleUrl: './line-chart.component.scss'
+})
+export class LineChartComponent {
+
+}
